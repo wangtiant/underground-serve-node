2a646c73e18f32ddae5c95bc62d35cfc34ade8a8
spring <847193449@qq.com>
2019-09-17 17:57:29 +0800

    登录生成token

--- app/api/v1/token.js
+++ app/api/v1/token.js
@@ -5,23 +5,30 @@ const router = new Router({
 const {LoginType} = require('../../lib/enum')
 const User = require('../../models/user')
 const { TokenException } = require('../../validators/validator')
+const { generateToken } = require('../../../core/util')
 
 router.post('/',async (ctx)=>{
     let v = await new TokenException().validate(ctx)
+    let token;
     switch (v.get('body.type')) {
         case LoginType.USER_ACCOUNT:
-            await accountLogin(v.get('body.account'),v.get('body.password'))
+             token = await accountLogin(v.get('body.account'),v.get('body.password'))
             break;
         case LoginType.USER_MOBILE:
 
             break;
         default:
+            throw new global.errors.ParameterException('没有相应的处理函数')
             break;
+    }
+    ctx.body = {
+        token
     }
 })
 
 async function accountLogin(account,password){
     const user = await User.verifyAccountPassword(account,password)
+    return token = generateToken(user.id, 2)
 }
 
 module.exports = router
--- config/config.js
+++ config/config.js
@@ -1,11 +1,15 @@
 module.exports = {
     environment:'dev',
     // environment:'prod',
-    database:{
-        dbName:'underground',
-        host:'localhost',
-        port:'3306',
-        user:'root',
+    database: {
+        dbName: 'underground',
+        host: 'localhost',
+        port: '3306',
+        user: 'root',
         password:'123456'
+    },
+    security: {
+        secretKey: 'abcdefg',
+        expiresIn: 60*60
     }
 }
